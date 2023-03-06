c11b49e0ddf6f371e798827004a1421732f1aeed
Alex Au <alex.au@health.nsw.gov.au>
2023-03-06 14:38:02 +1100

    added new special character dict

--- Split_and_Remove_Special_Characters/Remove_Special_Characters.py
+++ Split_and_Remove_Special_Characters/Remove_Special_Characters.py
@@ -2,6 +2,8 @@ ORU_File = "C:\\IS Migration Split Files\\Original_Zipped_Files\\Bulk-G\\Bulk-OR
 
 special_char_counts = {"\x07": 0, "\x08": 0, "\x0C": 0, "\x0B": 0}
 
+new_special_char_counts = {"\x07": 0, "\x08": 0, "\x0C": 0, "\x0B": 0}
+
 # Open the file for reading
 with open(ORU_File, 'r') as file:
     # Loop through each line in the file
@@ -26,7 +28,7 @@ with open(ORU_File, 'r') as file:
         file_content = file_content.replace(char, "")
 
 # Open the file for writing and write the modified string back to the file
-with open(oru_file_special_characters_removed, 'w') as file:
+with open(oru_file_special_characters_removed, 'w+') as file:
     file.write(file_content)
 
     # Loop through each line in the file
@@ -34,10 +36,10 @@ with open(oru_file_special_characters_removed, 'w') as file:
         # Loop through each character in the line
         for char in line:
             # Check if the character is one of the special characters
-            if char in special_char_counts:
+            if char in new_special_char_counts:
                 # If so, increment the count for that special character
-                special_char_counts[char] += 1
+                new_special_char_counts[char] += 1
 
 # Print the counts for each special character
-for char, count in special_char_counts.items():
+for char, count in new_special_char_counts.items():
     print(f"Found {count} occurrences of special character '{char}'.")
